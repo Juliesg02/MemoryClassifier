3663dda158242662778009657bf6277fe0cf401d
Julio Sanchez G <je02sg@gmail.com>
2024-12-09 12:29:01 +0100

    Initial project setup

---
+++ MemoryClassifier/MemoryClassifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryClassifier
+//
+//  Created by Julio Enrique Sanchez Guajardo on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryClassifier/MemoryClassifier/MemoryClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryClassifierApp.swift
+//  MemoryClassifier
+//
+//  Created by Julio Enrique Sanchez Guajardo on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
