9134d0c56064661448a634e9bf75cdf19d6b542e
Julio Sanchez G <je02sg@gmail.com>
2024-12-13 09:08:49 +0100

    Creation of BlockView design with fixed info

---
+++ MemoryClassifier/MemoryClassifier/BlockView.swift
@@ -0,0 +1,18 @@
+//
+//  BlockView.swift
+//  MemoryClassifier
+//
+//  Created by Julio Enrique Sanchez Guajardo on 12/12/24.
+//
+
+import SwiftUI
+
+struct BlockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlockView()
+}
